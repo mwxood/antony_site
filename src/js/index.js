6acfe354459f0d330738895eeae42aa846c77f0c
Mihail <mwxood@gmail.com>
2021-05-11 16:34:26 +0300

    'category'

--- src/js/index.js
+++ src/js/index.js
@@ -4,11 +4,13 @@ import slider from "./modules/slider";
 import scrollEffects from "./modules/scrollEffects";
 import gallery from "./modules/gallery";
 import tabs from "./modules/tabs";
+import customSelect from "./modules/customSelect";
 
 document.addEventListener('DOMContentLoaded', function () {
     navigation();
     scrollEffects();
     slider();
+    customSelect();
     tabs({
         tabsNav: '.tab-nav a',
         tabsContent: '.tab',
