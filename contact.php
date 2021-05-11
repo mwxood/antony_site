5870f56a6f63294ff0844fce1159918323fe51fa
Mihail <mwxood@gmail.com>
2021-05-11 21:51:50 +0300

    'category'

--- contact.php
+++ contact.php
@@ -75,6 +75,17 @@
                        </div>
                    </form>
                </div>
+            </div>
+
+            <div class="company-info-container">
+                <span class="company-logo">
+                    <img src="assets/img/antony-logo.png" alt="company logo">
+                    <span class="company-logo-slogan">Официален дистрибутор на ECE и KIRLANGIC за България</span>
+                </span>
+
+                <div class="company-text">
+                    <p>Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.</p>
+                </div><!--end -->
             </div><!--end company-info-container-->
         </section>
     </main>
