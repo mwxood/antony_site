814a685994ff965fc3a6faedf59f6a11e2277524
Mihail <mwxood@gmail.com>
2021-06-01 17:08:25 +0300

    fixed

---
+++ product-new.php
@@ -0,0 +1,298 @@
+<?php include('includes/header.php'); ?>
+
+<main class="content">
+    <section class="product-section category-section inner-section">
+
+        <div class="product-new">
+
+            <div class="product-new-heading">
+                <ul class="breadcrumbs">
+                    <li><a href="/">Начало</a></li>
+                    <li><a href="category.php">Продукти</a></li>
+                    <li>ECE</li>
+                </ul>
+
+                <h1>
+                    Черни маслини с костилка "Gemlik"
+                </h1>
+            </div><!--end product-new-heading-->
+
+            <div class="product-new-image-holder">
+                <a href="assets/img/maslini.png" class="gallery-img">
+                    <img src="assets/img/maslini.png" alt="maslini" />
+                </a>
+            </div>
+
+            <div class="product-new-about-aslini">
+               
+            
+
+            <div class="product-title ">
+                <h2>
+                    <span class="product-logo">
+                        <img data-src="assets/img/ece-about-logo.png" alt="ece logo">
+                    </span>Маслини Ece
+                </h2>
+                <span class="title-desc">
+                    “Антоний” ЕООД е единственият официален представител на турската фирма “ JENICAG GIDA SAM JIK A . S ” за България и вносител на над 50 вида хранителни продукти с търговската марка ЕСЕ.
+                </span>
+            </div>
+            </div>
+
+            
+        </div>
+        <!--end custom-prod-row-->
+
+        <div class="category-new-header">
+            <div class="custom-container">
+                <span class="category-new-title">Още продукти</span>
+            </div>
+        </div>
+
+        <div class="scroll-product">
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+
+            <div class="prod-scroll">
+                <a href="product.php" class="prod">
+                    <span class="prod-image">
+                        <img data-src="assets/img/prod.png" alt="Product">
+                    </span>
+                    <span class="prod-desc">
+                        Черни маслини <br> без костилка
+                    </span>
+                </a>
+            </div>
+            <!--end prod-scroll-->
+        </div>
+
+
+        <div class="company-info-container">
+            <span class="company-logo">
+                <img data-src="assets/img/antony-logo.png" alt="company logo">
+                <span class="company-logo-slogan">Официален дистрибутор на ECE и KIRLANGIC за България</span>
+            </span>
+
+            <div class="company-text">
+                <p>Lorem Ipsum е елементарен примерен текст, използван в печатарската и типографската индустрия. Lorem Ipsum е индустриален стандарт от около 1500 година, когато неизвестен печатар взема няколко печатарски букви и ги разбърква, за да напечата с тях книга с примерни шрифтове. Този начин не само е оцелял повече от 5 века, но е навлязъл и в публикуването на електронни издания като е запазен почти без промяна. Популяризиран е през 60те години на 20ти век със издаването на Letraset листи, съдържащи Lorem Ipsum пасажи, популярен е и в наши дни във софтуер за печатни издания като Aldus PageMaker, който включва различни версии на Lorem Ipsum.</p>
+            </div>
+            <!--end -->
+        </div>
+        <!--end company-info-container-->
+
+    </section>
+</main>
+<?php include('includes/footer.php'); ?>
